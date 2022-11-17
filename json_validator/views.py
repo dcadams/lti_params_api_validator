64e15c86ba4af115fdd46a8062918a5498774735
Arpit Gandhi <arpit.gandhi@xoriant.com>
2022-11-17 16:39:37 +0530

    Add basic validation

--- json_validator/views.py
+++ json_validator/views.py
@@ -2,12 +2,44 @@ import json
 
 from django.shortcuts import render
 from django.views import View
+from django.conf import settings
+import traceback
+
+
+def get_configuration():
+    """
+    Returns JSON object as a dictionary.
+    """
+    try:
+        with open(settings.CONFIG_PATH, encoding='utf-8') as conf_file:
+            conf_json = json.load(conf_file)
+    except Exception:
+        traceback.print_exc()
+
+    return conf_json
+
 
 class ValidateData(View):
     def post(self, request):
         json_data = json.loads(request.body)
+        conf = get_configuration()
+        validation = {}
         for block in json_data:
-            self.validate_block(block)
+            vald = self.validate_block(block, conf)
+            if vald:
+                validation[block['block_key']] = vald
 
-    def validate_block(self, block):
-        pass
+        print(validation)
+
+    def validate_block(self, block, conf):
+        error_lst = []
+        if not block['launch_url'] in conf['LAUNCH_URL']:
+            error_lst.append('launch_url')
+        if not block['tool_id'] in conf['TOOL_ID']:
+            error_lst.append('tool_id')
+        if not block['send_email'] == conf['SEND_EMAIL']:
+            error_lst.append('tool_id')
+        if not block['send_name'] == conf['SEND_NAME']:
+            error_lst.append('send_name')
+        
+        return error_lst
--- lti_params_api_validator/settings.py
+++ lti_params_api_validator/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CONFIG_PATH = 'lti_params_api_validator/config.json'
